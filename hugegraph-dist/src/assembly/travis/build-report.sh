ac23f6b675771402657e470c8362dc2c0dbd20e9
liningrui <liningrui@baidu.com>
2021-03-08 15:02:14 +0800

    test jacoco
    Change-Id: Ibfad4ff59c518916ae9cc3273e6a1311c52086a9

--- hugegraph-dist/src/assembly/travis/build-report.sh
+++ hugegraph-dist/src/assembly/travis/build-report.sh
@@ -5,5 +5,8 @@ set -ev
 cd hugegraph-test
 mvn jacoco:dump@pull-test-data -Dapp.host=localhost -Dapp.port=36320 -Dskip.dump=false
 cd ../
+
+ll hugegraph-*/target/classes/com/baidu/hugegraph
+
 java -jar $TRAVIS_DIR/jacococli.jar report hugegraph-test/target/jacoco-it.exec \
-     --classfiles hugegraph-api/target/classes/com/baidu/hugegraph --xml report.xml
+     --classfiles hugegraph-*/target/classes/com/baidu/hugegraph --xml report.xml
